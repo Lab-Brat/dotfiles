7e9aa1d4527ee22e3e3f479a42b0044a4ee7574c
Lab-Brat <xoovoox@protonmail.com>
2023-05-13 14:01:44 +0300

    add yaml to treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "vim", "vimdoc", "markdown", "python", "rust", "bash" },
+  ensure_installed = { "lua", "vim", "vimdoc", 
+	"markdown", "yaml", 
+	"python", "rust", "bash" },
   sync_install = false,
   auto_install = true,
   highlight = {
