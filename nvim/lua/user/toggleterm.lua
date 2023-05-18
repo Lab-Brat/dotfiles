947f41c519b565bc1016134d1d19148f33cc76f1
Lab-Brat <xoovoox@protonmail.com>
2023-05-18 23:19:35 +0300

    remove unnecessary configs

--- nvim/lua/user/toggleterm.lua
+++ nvim/lua/user/toggleterm.lua
@@ -1,6 +1,5 @@
 local M = {
   "akinsho/toggleterm.nvim",
-  commit = "19aad0f41f47affbba1274f05e3c067e6d718e1e",
   event = "VeryLazy",
 }
 
@@ -26,24 +25,6 @@ function M.config()
       border = "curved",
     },
   }
-
-  function _G.set_terminal_keymaps()
-    local opts = { noremap = true }
-    -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-  end
-
-  vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
-
-  local Terminal = require("toggleterm.terminal").Terminal
-  local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
-
-  function _LAZYGIT_TOGGLE()
-    lazygit:toggle()
-  end
 end
 
 return M
