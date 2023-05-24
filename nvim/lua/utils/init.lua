6193658a1ef01c769dfe2bd2ed2964f61cbb135d
Lab-Brat <xoovoox@protonmail.com>
2023-05-24 09:45:48 +0300

    turn off yamlls due to a bug

--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -8,7 +8,9 @@ M.servers = {
   "pyright",
   "bashls",
   "jsonls",
-  "yamlls",
+  -- "yamlls",
+  -- commenting out yaml language server
+  -- https://github.com/adrienverge/yamllint/issues/178
 }
 
 return M
